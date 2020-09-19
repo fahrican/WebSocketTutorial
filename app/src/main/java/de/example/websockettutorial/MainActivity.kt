ac2c44f05e6d50c2644e0a8e20ed9cd9a494a3eb
Fahri MacBook <fahri-can@web.de>
2020-09-19 21:09:04 +0200

    First part of the tutorial.

---
+++ app/src/main/java/de/example/websockettutorial/MainActivity.kt
@@ -0,0 +1,79 @@
+package de.example.websockettutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
+import org.json.JSONException
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URI
+import javax.net.ssl.SSLSocketFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webSocketClient: WebSocketClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initWebSocket()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        webSocketClient.close()
+    }
+
+    private fun initWebSocket() {
+        val coinbaseUri: URI? = URI(WEB_SOCKET_URL)
+
+        createWebSocketClient(coinbaseUri)
+    }
+
+    private fun createWebSocketClient(coinbaseUri: URI?) {
+        webSocketClient = object : WebSocketClient(coinbaseUri) {
+
+            override fun onOpen(handshakedata: ServerHandshake?) {
+                Log.d(TAG, "onOpen")
+                subscribe()
+            }
+
+            override fun onMessage(message: String?) {
+                Log.d(TAG, "onMessage: $message")
+                setUpBtcPriceText(message)
+            }
+
+            override fun onClose(code: Int, reason: String?, remote: Boolean) {
+                Log.d(TAG, "onClose")
+                unsubscribe()
+            }
+
+            override fun onError(ex: Exception?) {
+                Log.e("createWebSocketClient", "onError: ${ex?.message}")
+            }
+
+        }
+    }
+
+    private fun subscribe() {
+        webSocketClient.send(
+            "{\n" +
+                    "    \"type\": \"subscribe\",\n" +
+                    "    \"channels\": [{ \"name\": \"ticker\", \"product_ids\": [\"BTC-EUR\"] }]\n" +
+                    "}"
+        )
+    }
+
+    companion object {
+        const val WEB_SOCKET_URL = "wss://ws-feed.pro.coinbase.com"
+        const val TAG = "Coinbase"
+    }
+
+}
